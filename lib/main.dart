a5f0465369064b5557cc62a55b9fef25217ad4fe
DennisYun <2008dennisyun@gmail.com>
2023-02-03 13:16:20 +0900

    2. Static Wallet Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,147 @@
 import "package:flutter/material.dart";
+import 'package:fluttertutor/widgets/buttton.dart';
+import 'package:fluttertutor/widgets/currency_card.dart';
 
 void main() {
-  runApp(App());
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter"),
-        ),
-        body: Center(
-          child: Text("Hello Flutter"),
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "안녕, 데니스",
+                        style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "돌아온 걸 환영해",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                "네 전 재산",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(.8),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "\$ 194 482 000",
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(
+                    text: "Transfer",
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: "Request",
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    "Wallets",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    "View All",
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(.8), fontSize: 15),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const CurrencyCard(
+                name: "Euro",
+                code: "EUR",
+                amount: "6 428",
+                icon: Icons.euro_sharp,
+                isInverted: false,
+              ),
+              Transform.translate(
+                offset: const Offset(0, -20),
+                child: const CurrencyCard(
+                  name: "Bitcoin",
+                  code: "BTN",
+                  amount: "10 000",
+                  icon: Icons.currency_bitcoin,
+                  isInverted: true,
+                ),
+              ),
+              Transform.translate(
+                offset: const Offset(0, -40),
+                child: const CurrencyCard(
+                  name: "US Dollar",
+                  code: "USD",
+                  amount: "2 023",
+                  icon: Icons.attach_money_outlined,
+                  isInverted: false,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
