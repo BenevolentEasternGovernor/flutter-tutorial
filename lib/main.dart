2a7f773e699a691e475d9cd7b581b994a487d690
DennisYun <2008dennisyun@gmail.com>
2023-02-03 13:59:32 +0900

    4. BuildContext practice with tiny Life Cycle of Widgets practice

--- lib/main.dart
+++ lib/main.dart
@@ -12,54 +12,58 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: Scaffold(
+          body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            MyLargeTitle(),
+          ],
+        ),
+      )),
+    );
+  }
+}
 
-  void Function() onClicked(int add) {
-    return () {
-      setState(() {
-        counter += counter + add >= 0 ? add : 0;
-      });
-    };
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print("initState!!");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose!!");
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "click count",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                "$counter",
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    iconSize: 40,
-                    onPressed: onClicked(1),
-                    icon: const Icon(Icons.add_box_rounded),
-                  ),
-                  IconButton(
-                    iconSize: 40,
-                    onPressed: onClicked(-1),
-                    icon: const Icon(Icons.indeterminate_check_box_rounded),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
+    return Text(
+      "My Large Title",
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
